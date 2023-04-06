04a3e60e012fbf68d0bf20d3bd4c7fb4ee2b7684
Cristel Chandre <cchandre@gmail.com>
2023-04-06 10:32:13 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -4,17 +4,31 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pyhamsys',
-      version='0.0.1',
+      version='0.0.2',
       description='Some tools for Hamiltonian systems',
       long_description=long_description,
       long_description_content_type="text/markdown",
       packages=setuptools.find_packages(),
       url='http://github.com/cchandre/pyhamsys',
-      python_requires='>=3.8',
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Astronomy'
+     ],
+      python_requires='>=3.6',
       author='Cristel Chandre',
       author_email='cristel.chandre@cnrs.fr',
       license='BSD',
       ppy_modules=['pyhamsys'],
       package_dir={'':'pyhamsys/src'}, 
-      install_requires=[]
+      install_requires=[
+        "numpy",
+        "scipy"]
       )

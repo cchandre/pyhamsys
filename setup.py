213d441ee338b515a60390414dc8d13f1de95c05
Cristel Chandre <cchandre@gmail.com>
2024-04-15 12:11:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyhamsys',
-    version='0.2',
+    version='0.3',
     description='Some tools for Hamiltonian systems',
     long_description=long_description,
     long_description_content_type="text/markdown",
